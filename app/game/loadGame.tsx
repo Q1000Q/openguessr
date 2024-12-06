a11c6b9260ca8c6f3ab46f021eb5fbca01d57ad0
Q1000Q <1000q.mj@gmail.com>
2024-12-06 23:35:23 +0100

    Added main menu

---
+++ app/game/loadGame.tsx
@@ -0,0 +1,3 @@
+export default async function LoadGame(rounds: Number, time: Number) {
+    
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,101 +1,60 @@
-import Image from "next/image";
+"use client"
+import "./page.css"
+import LoadGame from "./game/loadGame";
+import { useState } from "react";
 
 export default function Home() {
+
+  const [rounds, setRounds] = useState(5);
+  const [time, setTime] = useState(180);
+
+  const decreseRounds = async () => {
+    if (rounds > 1) {
+      setRounds(rounds - 1);
+    }
+  }
+  
+  const increseRounds = async () => {
+    if (rounds < 100) {
+      setRounds(rounds + 1);
+    }
+  }
+  
+  const decreseTime = async () => {
+    if (time > 30) {
+      setTime(time - (time%30 == 0 ? 30 : time%30));
+    }
+  }
+  
+  const increseTime = async () => {
+    if (time < 3000) {
+      setTime(time + 30 - time%30);
+    }
+  }
+
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+        <div className="grid grid-cols-3 grid-rows-2 gap-8">
+          <button onClick={() => LoadGame(rounds, time)} className="bg-zinc-600 p-4 px-32 rounded hover:bg-zinc-800 transform transition-colors duration-200 col-span-2">Play Game</button>
+          <div>
+            <h2 className="text-center bg-zinc-600 rounded-t">Rounds</h2>
+            <div className="flex">
+              <button className="bg-zinc-600 p-2 px-4 rounded-bl hover:bg-zinc-800 transform transition-colors duration-200" onClick={decreseRounds}>-</button>
+              <input onChange={(e) => setRounds(Number(e.target.value))} type="number" name="rounds" id="rounds" value={rounds} className="text-black px-6 py-2 text-center w-full" />
+              <button className="bg-zinc-600 p-2 px-4 rounded-br hover:bg-zinc-800 transform transition-colors duration-200" onClick={increseRounds}>+</button>
+            </div>
+          </div>
+          <div>
+              <h2 className="text-center bg-zinc-600 rounded-t">Time Per Round (seconds)</h2>
+              <div className="flex">
+                <button className="bg-zinc-600 p-2 px-4 rounded-bl hover:bg-zinc-800 transform transition-colors duration-200" onClick={decreseTime}>-</button>
+                <input onChange={(e) => setTime(Number(e.target.value))} type="number" name="time" id="time" value={time} className="text-black px-6 py-2 text-center w-full" />
+                <button className="bg-zinc-600 p-2 px-4 rounded-br hover:bg-zinc-800 transform transition-colors duration-200" onClick={increseTime}>+</button>
+              </div>
+            </div>
         </div>
       </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
     </div>
   );
 }
