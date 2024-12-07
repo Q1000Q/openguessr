05ae0ced55936ac242b5eb79ac318c31f7bb1610
Q1000Q <1000q.mj@gmail.com>
2024-12-07 23:09:03 +0100

    Added menu <=> game integrations

--- app/game/page.tsx
+++ app/game/page.tsx
@@ -8,12 +8,23 @@ const render = (status: Status) => {
   return <div />;
 };
 
-const WrappedStreetViewMap = () => {
+interface GameProps {
+  rounds: number;
+  time: number;
+}
+
+const Game = ({ rounds, time }: GameProps) => {
   return (
-    <Wrapper apiKey={process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY || ""} render={render}>
-      <StreetViewMap/>
-    </Wrapper>
+    <div className='h-[100vh] w-full'>
+      <button onClick={() => location.reload()} className='absolute z-50 top-4 left-4 bg-black/70 p-4 rounded-full'><svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 text-white" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M10 19l-7-7m0 0l7-7m-7 7h18" /></svg></button>
+      <div className='absolute top-2 z-50 left-1/2 transform -translate-x-1/2 text-3xl bg-black/70 py-4 px-16 rounded-xl'>
+        {`${Math.floor(time / 60).toString().padStart(2, '0')}:${(time % 60).toString().padStart(2, '0')}`}
+      </div>
+      <Wrapper apiKey={process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY || ""} render={render}>
+        <StreetViewMap/>
+      </Wrapper>
+    </div>
   );
 };
 
-export default WrappedStreetViewMap;
+export default Game;
--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,13 @@
 "use client"
 import "./page.css"
 import { useState } from "react";
-import { redirect } from "next/navigation";
+import Game from "./game/page";
 
 export default function Home() {
 
   const [rounds, setRounds] = useState(5);
   const [time, setTime] = useState(180);
+  const [view, setView] = useState("home");
 
   const decreseRounds = async () => {
     if (rounds > 1) {
@@ -32,11 +33,11 @@ export default function Home() {
     }
   }
 
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+  return (view == "home" ? 
+    (<div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
         <div className="grid grid-cols-3 grid-rows-2 gap-8">
-          <button onClick={() => redirect("/game")} className="bg-zinc-600 p-4 px-32 rounded hover:bg-zinc-800 transform transition-colors duration-200 col-span-2">Play Game</button>
+          <button onClick={() => setView("game")} className="bg-zinc-600 p-4 px-32 rounded hover:bg-zinc-800 transform transition-colors duration-200 col-span-2">Play Game</button>
           <div className="grid grid-cols-[1fr_6fr_1fr] grid-rows-[2fr_3fr]">
             <h2 className="text-center bg-zinc-600 rounded-t col-span-3">Rounds</h2>
             <button className="bg-zinc-600 rounded-bl hover:bg-zinc-800 transition-colors duration-200" onClick={decreseRounds}>-</button>
@@ -51,6 +52,6 @@ export default function Home() {
           </div>
         </div>
       </main>
-    </div>
+    </div>) : view == "game" ? <Game rounds={rounds} time={time}></Game> : ""
   );
 }
