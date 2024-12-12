ec439a0f3df49a344668862cf33f740cd0c99e08
Q1000Q <1000q.mj@gmail.com>
2024-12-12 20:00:43 +0100

    Changed style of Play Game button

--- app/page.tsx
+++ app/page.tsx
@@ -55,7 +55,7 @@ export default function Home() {
     (<div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
         <div className="grid grid-cols-3 grid-rows-2 gap-8">
-          <button onClick={() => setView("game")} className="bg-zinc-600 p-4 px-32 rounded hover:bg-zinc-800 transform transition-colors duration-200 col-span-2">Play Game</button>
+          <button onClick={() => setView("game")} className="bg-zinc-600 p-4 px-32 rounded hover:bg-zinc-800 transform transition-colors duration-200 col-span-2 font-bold text-xl">Play Game</button>
           <div className="grid grid-cols-[1fr_6fr_1fr] grid-rows-[2fr_3fr]">
             <h2 className="text-center bg-zinc-600 rounded-t col-span-3">Rounds</h2>
             <button className="bg-zinc-600 rounded-bl hover:bg-zinc-800 transition-colors duration-200" onClick={decreseRounds}>-</button>
