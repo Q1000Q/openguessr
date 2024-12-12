2bd6d1f1b7d3d8cd687cd583210a2223646ff48b
Q1000Q <1000q.mj@gmail.com>
2024-12-12 20:50:50 +0100

    Slightly rounded inputs in main page

--- app/page.tsx
+++ app/page.tsx
@@ -59,7 +59,7 @@ export default function Home() {
           <div className="grid grid-cols-[1fr_6fr_1fr] grid-rows-[2fr_3fr]">
             <h2 className="text-center bg-zinc-600 rounded-t col-span-3">Rounds</h2>
             <button className="bg-zinc-600 rounded-bl hover:bg-zinc-800 transition-colors duration-200" onClick={decreseRounds}>-</button>
-            <input onChange={(e) => setRounds(Number(e.target.value))} type="number" name="rounds" id="rounds" value={rounds} className="text-black text-center w-full" />
+            <input onChange={(e) => setRounds(Number(e.target.value))} type="number" name="rounds" id="rounds" value={rounds} className="text-black text-center w-full rounded-t-sm" />
             <button className="bg-zinc-600 rounded-br hover:bg-zinc-800 transition-colors duration-200" onClick={increseRounds}>+</button>
           </div>
           <div className="bg-zinc-600 rounded flex justify-center items-center">
@@ -82,7 +82,7 @@ export default function Home() {
           <div className="grid grid-cols-[1fr_6fr_1fr] grid-rows-[2fr_3fr]">
             <h2 className="text-center bg-zinc-600 rounded-t col-span-3">Time Per Round (seconds)</h2>
             <button className="bg-zinc-600 rounded-bl hover:bg-zinc-800 transition-colors duration-200" onClick={decreseTime}>-</button>
-            <input onChange={(e) => setTime(Number(e.target.value))} type="number" name="time" id="time" value={time} className="text-black text-center w-full" />
+            <input onChange={(e) => setTime(Number(e.target.value))} type="number" name="time" id="time" value={time} className="text-black text-center w-full rounded-t-sm" />
             <button className="bg-zinc-600 rounded-br hover:bg-zinc-800 transition-colors duration-200" onClick={increseTime}>+</button>
           </div>
         </div>
